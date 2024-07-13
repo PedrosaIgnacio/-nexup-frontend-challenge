9008c10e3ca2f8f4094b3811f79c96b253db18b9
PedrosaIgnacio <ignaciopedrosa1998@gmail.com>
2024-07-13 14:46:18 -0300

    fix(product-status): Change color when product is active or inactive

--- src/components/ProductRow.tsx
+++ src/components/ProductRow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Product } from '../models/Product';
 import { formatCurrency } from '../utils/currency';
+import { ProductStatus } from '../models/ProductStatus';
 
 interface ProductRowProps {
   product: Product;
@@ -8,7 +9,7 @@ interface ProductRowProps {
 
 interface ProductCellProps {
   property: keyof Product;
-  value: any;
+  value: string | number;
 }
 
 type ColSpanSizeType = {
@@ -25,24 +26,45 @@ const COL_SPAN_SIZES: ColSpanSizeType = {
 
 const ProductCell: React.FC<ProductCellProps> = (props) => {
   const { property, value } = props;
-  return (
-    <div
-      className={`col-span-${COL_SPAN_SIZES[property]} py-2 px-2 sm:px-6 items-center flex`}
-    >
-      {property === 'price' ? (
-        <p className="capitalize tracking-tight text-xs sm:text-sm">
-          {formatCurrency(value, 'USD')}
-        </p>
-      ) : (
-        <p className="capitalize tracking-tight text-xs sm:text-sm">{value}</p>
-      )}
-    </div>
-  );
+  switch (property) {
+    case 'price':
+      return (
+        <div
+          className={`col-span-${COL_SPAN_SIZES[property]} py-2 px-2 sm:px-6 items-center flex`}
+        >
+          <p className="capitalize tracking-tight text-xs sm:text-sm">
+            {formatCurrency(value as number, 'USD')}
+          </p>
+        </div>
+      );
+    case 'status':
+      return (
+        <div
+          className={`col-span-${COL_SPAN_SIZES[property]} py-2 px-2 sm:px-6 items-center flex`}
+        >
+          <p
+            className={`${value === ProductStatus.Active ? 'bg-blue-400 rounded-full px-2 py-1 text-white font-bold text-xs' : 'bg-gray-400 rounded-full px-2 py-1 text-white font-bold text-xs'} capitalize tracking-tight`}
+          >
+            {value}
+          </p>
+        </div>
+      );
+    default:
+      return (
+        <div
+          className={`col-span-${COL_SPAN_SIZES[property]} py-2 px-2 sm:px-6 items-center flex`}
+        >
+          <p className="capitalize tracking-tight text-xs sm:text-sm">
+            {value}
+          </p>
+        </div>
+      );
+  }
 };
 
 export const ProductRow: React.FC<ProductRowProps> = ({ product }) => (
   <div
-    className="col-span-10 grid grid-cols-10 rounded-sm bg-white my-[0.15rem]"
+    className="col-span-10 grid grid-cols-10 rounded-lg bg-white my-[0.15rem] hover:bg-[#F7F7F7]"
     key={product.id}
   >
     {Object.keys(product)
